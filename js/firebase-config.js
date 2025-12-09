87d9c971cbeaa61088935f46d750f22b1d83294c
margottamarco93-cmyk <margotta.marco93@gmail.com>
2025-12-09 14:27:51 +0100

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// js/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDWisOm9z0LrSV0BczsKD5qYuVfswwT7jw",
+  authDomain: "progetto-cooperativa-passioni.firebaseapp.com",
+  projectId: "progetto-cooperativa-passioni",
+  storageBucket: "progetto-cooperativa-passioni.firebasestorage.app",
+  messagingSenderId: "578783174328",
+  appId: "1:578783174328:web:9770c86aa93d7fa3055e4d"
+};
